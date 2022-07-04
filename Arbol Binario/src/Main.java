101bb3aa066c7b02ae52c18f91197357209087d4
cynicallrex <nawelmisha@gmail.com>
2022-07-04 10:19:16 -0400

    ARBOL BINARIO

---
+++ Arbol Binario/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda();
+
+        abb.insertar(3);
+        abb.insertar(2);
+        abb.insertar(4);
+        abb.insertar(1);
+        abb.insertar(3);
+        abb.insertar(5);
+        abb.insertar(7);
+        abb.insertar(6);
+
+        abb.mostrarInOden();
+
+        abb.insertar(8);
+        abb.insertar(11);
+        abb.insertar(21);
+
+        abb.mostrarInOden();
+    }
+}
